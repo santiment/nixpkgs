8caff508f05e8cc0d0a2076b473838cc037111d6
tsanko@gmail.com <tsanko@gmail.com>
2017-10-11 16:28:12 +0300

    Add autoupgrade and fix amazon-init
    Currently autoupgrade follows the following schedule:
    
    production - once a day at 00:00:00 UTC
    stage - once every 10 minutes
    Every other deployment environment - no autoupgrade

--- nixos/modules/santiment/default.nix
+++ nixos/modules/santiment/default.nix
@@ -26,6 +26,94 @@ in {
       "nixos-config=/etc/nixos/configuration.nix"
       "ssh-config-file=/etc/nixos/secrets/sshconfig"
     ];
+
+    /* Set-up autoupgrade */
+    environment.systemPackages = [
+      pkgs.gzip
+    ];
+    
+    system.autoUpgrade = {
+      /* Enable for the stage and production environments */
+      enable = (
+        pkgs.deploymentEnvironment == "stage" ||
+        pkgs.deploymentEnvironment == "production"
+      );
+
+      flags = concatMap (x: ["-I" x]) config.nix.nixPath;
+
+
+      /* Upgrade the production environment once a day at 00:00:00 UTC.
+         Upgrade all other environments once every 10 minutes
+      */
+      dates = if pkgs.deploymentEnvironment == "production"
+        then
+          "daily UTC"
+        else
+          "*:00/10:00";
+    };
+
+    systemd.services.nixos-upgrade.path = [pkgs.gzip];
+
+    /* Initialize system from user-data on startup */
+    systemd.services.amazon-custom-init = {
+      description = "Initialize system from EC2 userdata on startup";
+
+      wantedBy = [ "multi-user.target" ];
+      after = [ "multi-user.target" ];
+      requires = [ "network-online.target" ];
+
+      restartIfChanged = false;
+      unitConfig.X-StopOnRemoval = false;
+
+      serviceConfig = {
+	Type = "oneshot";
+	RemainAfterExit = true;
+      };
+
+      environment = {
+        NIX_PATH = concatStringsSep ":" config.nix.nixPath;
+      };
+
+      script = ''
+	#!${pkgs.stdenv.shell} -eu
+
+	echo "attempting to fetch configuration from EC2 user data..."
+
+	export HOME=/root
+	export PATH=${pkgs.lib.makeBinPath [ config.nix.package pkgs.systemd pkgs.gnugrep pkgs.gnused config.system.build.nixos-rebuild]}:$PATH
+
+	userData=/etc/ec2-metadata/user-data
+
+	if [ -s "$userData" ]; then
+	  # If the user-data looks like it could be a nix expression,
+	  # copy it over. Also, look for a magic three-hash comment and set
+	  # that as the channel.
+	  if sed '/^\(#\|SSH_HOST_.*\)/d' < "$userData" | grep -q '\S'; then
+	    channels="$(grep '^###' "$userData" | sed 's|###\s*||')"
+	    printf "%s" "$channels" | while read channel; do
+	      echo "writing channel: $channel"
+	    done
+
+	    if [[ -n "$channels" ]]; then
+	      printf "%s" "$channels" > /root/.nix-channels
+	      nix-channel --update
+	    fi
+
+	    echo "setting configuration from EC2 user data"
+	    cp "$userData" /etc/nixos/configuration.nix
+	  else
+	    echo "user data does not appear to be a Nix expression; ignoring"
+	    exit
+	  fi
+	else
+	  echo "no user data is available"
+	  exit
+	fi
+
+	nixos-rebuild switch
+      '';
+
+    };
     
   };
  
